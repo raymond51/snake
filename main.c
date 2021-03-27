fa1b36108a88a139a07f9248eb905a6689b9b446
raymond51 <rzhen.qmfs@gmail.com>
2021-03-27 16:53:50 +0000

    feat: add getInput()

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define DEBUG_PRINT
+#define QUIT 'q'
 
 typedef struct Food
 {
@@ -12,12 +16,12 @@ typedef struct Food
 
 typedef struct Snake
 {
-
     bool isRunning;
-
+    Food food;
 } Snake;
 
 bool init_game(Snake *g);
+void getInput(Snake *g);
 void print_in_middle(int startx, int starty, int width, char *string, WINDOW *win);
 
 int main(int argc, char const *argv[])
@@ -28,26 +32,48 @@ int main(int argc, char const *argv[])
         /*Game loop*/
         while (game->isRunning)
         {
+            getInput(game);
+            usleep(32 * 1000); //FPS
         }
     }
+
+    free(game);
     endwin(); //end curses mode
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 bool init_game(Snake *g)
 {
     bool status = true;
-    (*g).isRunning = false;
+    (*g).isRunning = true;
     char *welcome_string = "Welcome to SNAKE! (Press Any Key to start)";
-    initscr();            //init curses screen and manipulation routines
-    cbreak();             //Immediate pass user input to program
+
+    initscr(); //init curses screen and manipulation routines
+    cbreak();  //Immediate pass user input to program
+    noecho();
     keypad(stdscr, TRUE); //Enable arrow function keys
     curs_set(FALSE);      //hide cursors
 
     print_in_middle(0, LINES / 2, 0, welcome_string, NULL); //COLS - specify width of screen in characters
     getch();                                                //wait for user input
 
+#ifdef DEBUG_PRINT
+    printf("Game error\n");
+#endif
+
     return status;
+}
+
+void getInput(Snake *g)
+{
+    char c;
+    c = getch();
+    switch (c)
+    {
+    case QUIT:
+        (*g).isRunning = false;
+        break;
+    }
 }
 
 void print_in_middle(int startx, int starty, int width, char *string, WINDOW *win)
